84ad8989df50d8693fa72a79e44ac2deb852552e
Hamza DAMIRI <hamza.damiri@gmail.com>
2024-07-04 23:00:37 +0100

    Project update.

--- src/main/java/ma/dev7hd/projetfinaljeespringangulardigitalbanking/ProjetFinalJeeSpringAngularDigitalBankingApplication.java
+++ src/main/java/ma/dev7hd/projetfinaljeespringangulardigitalbanking/ProjetFinalJeeSpringAngularDigitalBankingApplication.java
@@ -2,23 +2,15 @@ package ma.dev7hd.projetfinaljeespringangulardigitalbanking;
 
 import ma.dev7hd.projetfinaljeespringangulardigitalbanking.dtos.BankAccountDTO;
 import ma.dev7hd.projetfinaljeespringangulardigitalbanking.dtos.CustomerDTO;
-import ma.dev7hd.projetfinaljeespringangulardigitalbanking.entities.*;
-import ma.dev7hd.projetfinaljeespringangulardigitalbanking.enumirats.AccountStatus;
-import ma.dev7hd.projetfinaljeespringangulardigitalbanking.enumirats.OperationType;
 import ma.dev7hd.projetfinaljeespringangulardigitalbanking.exceptions.BankAccountNotFoundException;
 import ma.dev7hd.projetfinaljeespringangulardigitalbanking.exceptions.CustomerNotFoundException;
 import ma.dev7hd.projetfinaljeespringangulardigitalbanking.exceptions.InsufficientBalanceException;
-import ma.dev7hd.projetfinaljeespringangulardigitalbanking.repositories.BankAccountRepository;
-import ma.dev7hd.projetfinaljeespringangulardigitalbanking.repositories.CustomerRepository;
-import ma.dev7hd.projetfinaljeespringangulardigitalbanking.repositories.OperationRepository;
 import ma.dev7hd.projetfinaljeespringangulardigitalbanking.services.BankAccountServiceImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -32,7 +24,7 @@ public class ProjetFinalJeeSpringAngularDigitalBankingApplication {
     @Bean
     CommandLineRunner commandLineRunner(BankAccountServiceImpl bankAccountService){
         return args -> {
-            Stream.of("Hamza", "Hind", "Khadija", "Mustapha").forEach(name ->{
+            Stream.of("Hamza", "Mohammed", "Khadija", "Mustapha").forEach(name ->{
                 CustomerDTO customerDTO = new CustomerDTO();
                 customerDTO.setName(name);
                 customerDTO.setEmail(name + "@gmail.com");
@@ -63,52 +55,4 @@ public class ProjetFinalJeeSpringAngularDigitalBankingApplication {
             });
         };
     }
-    //@Bean
-    CommandLineRunner start(CustomerRepository customerRepository,
-                            BankAccountRepository bankAccountRepository,
-                            OperationRepository operationRepository) {
-        return args -> {
-            Stream.of("Hamza", "Hind", "Khadija", "Mustapha").forEach(name -> {
-                Customer customer = new Customer();
-                customer.setName(name);
-                customer.setEmail(name + "@gmail.com");
-                customerRepository.save(customer);
-            });
-            customerRepository.findAll().forEach(customer -> {
-                CurrentAccount currentAccount = new CurrentAccount();
-                currentAccount.setBalance(Math.random() * 20000 + 5000);
-                currentAccount.setCurrency("MAD");
-                currentAccount.setStatus(AccountStatus.CREATED);
-                currentAccount.setCreatedAt(new Date());
-                currentAccount.setCustomer(customer);
-                currentAccount.setOverDraft(5000);
-                bankAccountRepository.save(currentAccount);
-            });
-
-            customerRepository.findAll().forEach(customer -> {
-                SavingAccount savingAccount = new SavingAccount();
-                savingAccount.setBalance(Math.random() * 20000 + 5000);
-                savingAccount.setCurrency("MAD");
-                savingAccount.setStatus(AccountStatus.CREATED);
-                savingAccount.setCreatedAt(new Date());
-                savingAccount.setCustomer(customer);
-                savingAccount.setInterestRate(4.5);
-                bankAccountRepository.save(savingAccount);
-            });
-
-            bankAccountRepository.findAll().forEach(account -> {
-                for (int i = 0; i < 5; i++) {
-                    Operation operation = new Operation();
-                    operation.setAccount(account);
-                    operation.setType(Math.random() > 0.5 ? OperationType.CREDIT : OperationType.DEBIT);
-                    Date date = new Date(2024, 6,15);
-                    operation.setDate(date);
-                    System.out.println(date);
-                    operation.setAmount(Math.random() * 10000 + 1000);
-                    operationRepository.save(operation);
-                }
-            });
-        };
-    }
-
 }
